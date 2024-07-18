a8e451232c537af11698c62cc1c8f55419169631
Viktoriya Gorlachuk <Sarama.Sab@yandex.ru>
2024-07-18 18:29:24 +0300

    add allure report new

--- src/test/java/AuthCourierApiTest.java
+++ src/test/java/AuthCourierApiTest.java
@@ -9,52 +9,46 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.empty;
 
-public class AuthCourierApiTest extends BaseUriParentClass {
+public class AuthCourierApiTestPositive extends BaseUriParentClass {
 
-    @Step("Авторизация курьером")
-    public void authCourierPositive(){
+    @Test
+    @DisplayName("Авторизация курьером")
+    @Description("Позитивная сквозная проверка на авторизацию")
+    public void postApiLogin(){
         AuthCourier authCourier = new AuthCourier("Angry333", "1234");
-        Response response =
-                given()
-                        .header("Content-type", "application/json")
-                        .body(authCourier)
-                        .when()
-                        .post("/api/v1/courier/login");
-        System.out.println("Тело переменной response метода authCourier " + response.asString());
-        response.then().statusCode(200).assertThat().body("id", not(empty()));
-        System.out.println("Метод authCourierPositive() выполнен успешно");
+        //создали переменную класса AuthCourier для данных авторизации
+        AuthCourierApiTestPositive getApi = new AuthCourierApiTestPositive();
+        //создали переменную класса AuthCourierApiTestPositive чтобы вызывать методы шагов
+        Response response = getApi.apiPost(authCourier);
+        //создали переменную, в которой будет храниться результат вызова шага apiPost(authCourier);
+        getApi.assertThatApiPositive(response);
+        //вызвали метод шага сравнения, положив в него данные из переменной response
     }
 
-    @Step("Авторизация курьером с левым логином")
+    @Test
+    @DisplayName("Негативная проверка - логин")
+    @Description("Авторизация с левым логином")
     public void authCourierWrongLogin(){
         AuthCourier courierWrongLogin = new AuthCourier("Ang", "1234");
-        Response response =
-                given()
-                        .header("Content-type", "application/json")
-                        .body(courierWrongLogin)
-                        .when()
-                        .post("/api/v1/courier/login");
-        System.out.println("Тело переменной response метода authCourierWrongLogin()r " + response.asString());
-        response.then().statusCode(404).assertThat().body("message", equalTo("Учетная запись не найдена"));
-        System.out.println("Метод authCourierWrongLogin() выполнен успешно");
+        AuthCourierApiTestPositive authCourierApiTestPositive = new AuthCourierApiTestPositive();
+        Response response = authCourierApiTestPositive.apiPost(courierWrongLogin);
+        authCourierApiTestPositive.assertThatApiWtfLoginAndPassword(response);
 
     }
 
-    @Step("Авторизация курьером с левым паролем")
+    @Test
+    @DisplayName("Негативная проверка - пароль")
+    @Description("Авторизация с левым паролем")
     public void authCourierWrongPass(){
         AuthCourier courierWrongPass = new AuthCourier("Angry", "12");
-        Response response =
-                given()
-                        .header("Content-type", "application/json")
-                        .body(courierWrongPass)
-                        .when()
-                        .post("/api/v1/courier/login");
-        System.out.println("Тело переменной response метода authCourierWrongPass() " + response.asString());
-        response.then().statusCode(404).assertThat().body("message", equalTo("Учетная запись не найдена"));
-        System.out.println("Метод authCourierWrongPass() выполнен успешно");
+        AuthCourierApiTestPositive authCourierApiTestPositive = new AuthCourierApiTestPositive();
+        Response response = authCourierApiTestPositive.apiPost(courierWrongPass);
+        authCourierApiTestPositive.assertThatApiWtfLoginAndPassword(response);
     }
 
-    @Step("Авторизация курьером с пустым полем ввода логина")
+    @Test
+    @DisplayName("Негативная проверка - пустой логин")
+    @Description("Авторизация с пустым полем ввода логина")
     public void authCourierEmptyLogin(){
         AuthCourier courierEmptyLogin = new AuthCourier("", "1234");
         Response response =
@@ -68,29 +62,43 @@ public class AuthCourierApiTest extends BaseUriParentClass {
         System.out.println("Метод authCourierEmptyLogin() выполнен успешно");
     }
 
-    @Step("Авторизация курьером с пустым полем ввода пароля")
+    @Test
+    @DisplayName("Негативная проверка - пустой пароль")
+    @Description("Авторизация курьером с пустым полем ввода пароля")
     public void authCourierEmptyPass(){
         AuthCourier courierEmptyPass = new AuthCourier("Angry", "");
+        AuthCourierApiTestPositive getApi = new AuthCourierApiTestPositive();
+        Response response = getApi.apiPost(courierEmptyPass);
+        getApi.assertThatApiEmptyPass(response);
+    }
+
+    @Step("Авторизация курьером - обращение к ручке")
+    public Response apiPost(AuthCourier courier){
         Response response =
                 given()
                         .header("Content-type", "application/json")
-                        .body(courierEmptyPass)
+                        .body(courier)
                         .when()
                         .post("/api/v1/courier/login");
-        System.out.println("Тело переменной response метода authCourierEmptyPass() " + response.asString());
+        System.out.println("Тело переменной response " + response.asString());
+        return response;
+    }
+
+    @Step("Сравнение ответа от сервера при авторизации с пустым паролем")
+    public void assertThatApiEmptyPass(Response response){
         response.then().statusCode(400).assertThat().body("message", equalTo("Недостаточно данных для входа"));
         System.out.println("Метод authCourierEmptyPass() выполнен успешно");
     }
 
-    @Test
-    @DisplayName("Авторизация курьером")
-    @Description("Позитивные и негативные проверки на поля авторизации")
-    public void authCourier(){
-        AuthCourierApiTest authCourier = new AuthCourierApiTest();
-        authCourier.authCourierPositive();
-        authCourier.authCourierWrongLogin();
-        authCourier.authCourierWrongPass();
-        authCourier.authCourierEmptyLogin();
-        authCourier.authCourierEmptyPass();
+    @Step("Сравнение ответа от сервера при авторизации с заполненными паролем и логином")
+    public void assertThatApiPositive(Response response){
+        response.then().statusCode(200).assertThat().body("id", not(empty()));
+        System.out.println("Метод authCourierPositive() выполнен успешно");
+    }
+
+    @Step("Сравнение ответа от сервера при авторизации с левым логином")
+    public void assertThatApiWtfLoginAndPassword(Response response){
+        response.then().statusCode(404).assertThat().body("message", equalTo("Учетная запись не найдена"));
+        System.out.println("Метод authCourierWrongLogin() выполнен успешно");
     }
 }
